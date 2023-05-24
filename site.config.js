e254151b590be4e8ea343c90a79bb604dfb8b276
AyaCaiyajie <117627006+AyaCaiyajie@users.noreply.github.com>
2023-05-24 16:49:27 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '6a76fdd9ec6d4ae2855aa790d620eec4',
+  rootNotionPageId: '03110ccceb234038a65a98cc3b3dfe70',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -21,7 +21,7 @@ module.exports = {
   github: 'frankcbliu',
   wechatPublicName: '菜饼不菜',
   wechatPublicURL: 'https://mp.weixin.qq.com/s?__biz=MzkxMzIyOTI1Mg==&mid=2247486027&idx=1&sn=2840f0b8cfdc982b9095dd18ec9892e5',
-  notionPublic: 'https://frankcbliu.notion.site/6a76fdd9ec6d4ae2855aa790d620eec4',
+  notionPublic: 'https://frankcbliu.notion.site/03110ccceb234038a65a98cc3b3dfe70',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
